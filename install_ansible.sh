a47498213ada3e7a735302f48f10588325eaa97d
Mark Vernon <vernon@mesatrail.com>
2015-10-14 11:04:45 -0500

    Work on local slave deploy

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,6 +4,8 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible unzip
 
+if [ -z "$1" ]; then
+if [ "$1" != "local" ]; then
 wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.3_linux_amd64.zip -O terraform_0.6.3.zip
 unzip terraform_0.6.3.zip -d terraform_0.6.3
 sudo cp -P terraform_0.6.3/terraform /usr/local/bin
@@ -11,5 +13,5 @@ sudo cp -P terraform_0.6.3/terraform-provider-aws /usr/local/bin
 sudo cp -P terraform_0.6.3/terraform-provider-google /usr/local/bin
 sudo cp -P terraform_0.6.3/terraform-provider-openstack /usr/local/bin
 sudo cp -P terraform_0.6.3/terraform-provisioner-local-exec /usr/local/bin
-
-
+fi
+fi
