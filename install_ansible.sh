cce9a9222fb34efc5c94166ef842f87f6513ac72
Mark Vernon <vernon@mesatrail.com>
2015-10-15 14:27:50 -0500

    Updating terraform to 0.6.4 Fixing typos.  Better sensu deploy

--- install_ansible.sh
+++ install_ansible.sh
@@ -6,12 +6,12 @@ sudo apt-get install -y ansible unzip
 
 if [ -z "$1" ]; then
 if [ "$1" != "local" ]; then
-wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.3_linux_amd64.zip -O terraform_0.6.3.zip
-unzip terraform_0.6.3.zip -d terraform_0.6.3
-sudo cp -P terraform_0.6.3/terraform /usr/local/bin
-sudo cp -P terraform_0.6.3/terraform-provider-aws /usr/local/bin
-sudo cp -P terraform_0.6.3/terraform-provider-google /usr/local/bin
-sudo cp -P terraform_0.6.3/terraform-provider-openstack /usr/local/bin
-sudo cp -P terraform_0.6.3/terraform-provisioner-local-exec /usr/local/bin
+wget https://dl.bintray.com/mitchellh/terraform/terraform_0.6.4_linux_amd64.zip -O terraform_0.6.4.zip
+unzip terraform_0.6.4.zip -d terraform_0.6.4
+sudo cp -P terraform_0.6.4/terraform /usr/local/bin
+sudo cp -P terraform_0.6.4/terraform-provider-aws /usr/local/bin
+sudo cp -P terraform_0.6.4/terraform-provider-google /usr/local/bin
+sudo cp -P terraform_0.6.4/terraform-provider-openstack /usr/local/bin
+sudo cp -P terraform_0.6.4/terraform-provisioner-local-exec /usr/local/bin
 fi
 fi
--- inventory/terraform.py
+++ inventory/terraform.py
@@ -526,7 +526,7 @@ def main():
                         help='with --list, exclude hostvars')
     default_root = os.environ.get('TERRAFORM_STATE_ROOT',
                                   os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                               '..', '..', )))
+                                                               '..' )))
     parser.add_argument('--root',
                         default=default_root,
                         help='custom root to search for `.tfstate`s in')
