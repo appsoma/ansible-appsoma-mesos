d785e5e69c29d2c2b43e33840f4ad0480054da11
Mark Vernon <vernon@mesatrail.com>
2015-10-20 11:42:09 -0500

    Scaler debug

--- update_cluster.bash
+++ update_cluster.bash
@@ -1,4 +1,3 @@
 #!/bin/bash
 source setup_amazon_env.bash $1
-ansible-playbook --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$1"
 ansible-playbook --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py $2 -e "cluster_name=$1"
