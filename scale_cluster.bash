7a230ffdab7dc5e1dcc5867ffcf5c5171732ba19
Mark Vernon <vernon@mesatrail.com>
2015-10-20 10:55:37 -0500

    Work on scaling

--- scale_cluster.bash
+++ scale_cluster.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 source setup_amazon_env.bash $1
-ansible-playbook --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$1"
+if [ -z "$2" ] ; then
+   plan="false"
+else
+   plan="true"
+fi
+ansible-playbook -v --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$1" -e "terraform_plan_only=$plan"
+
