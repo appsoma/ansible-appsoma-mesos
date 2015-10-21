543ceb3a0ef626392c91f04ed42edd9f8a68c53f
vernonm <mark.vernon@appsoma.com>
2015-10-21 08:37:19 -0500

    Bug fixes in mesos-dns. Improvements in local deploy

--- scale_cluster.bash
+++ scale_cluster.bash
@@ -1,9 +1,25 @@
 #!/bin/bash
-source setup_amazon_env.bash $1
-if [ -z "$2" ] ; then
-   plan="false"
+while [[ $# > 0 ]]
+do
+  key="$1"
+  case $key in
+    --plan_only)
+      PLAN_ONLY="true"
+    ;;
+    --local_deploy)
+      LOCAL_DEPLOY="true"
+    ;;
+    *)
+      CLUSTER_NAME=$key
+    ;;
+  esac
+  shift
+done
+
+source setup_amazon_env.bash $CLUSTER_NAME
+if [ -z "$PLAN_ONLY" ] ; then
+    echo ansible-playbook -v --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$1" -e "local_slave_deploy=$LOCAL_DEPLOY"
 else
-   plan="true"
+    echo ansible-playbook -v --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$1" -e "local_slave_deploy=$LOCAL_DEPLOY" -e "terraform_plan_only=true"
 fi
-ansible-playbook -v --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$1" -e "terraform_plan_only=$plan"
 
