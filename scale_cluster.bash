0c1781cdaa5921d1eafea10950f863a1dce37a00
Mark Vernon <vernon@mesatrail.com>
2015-10-23 13:34:47 -0500

    Cleanup scale_cluster

--- scale_cluster.bash
+++ scale_cluster.bash
@@ -18,8 +18,8 @@ done
 
 source setup_amazon_env.bash $CLUSTER_NAME
 if [ -z "$PLAN_ONLY" ] ; then
-    ansible-playbook -v --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY"
+    ansible-playbook -v --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pemmanage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY"
 else
-    ansible-playbook -v --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pem -i inventory/terraform.py manage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY" -e "terraform_plan_only=true"
+    ansible-playbook -v --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pem manage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY" -e "terraform_plan_only=true"
 fi
 
