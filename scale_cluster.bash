640c18b0dabd213032d200f2e9cfe8cb3148ad71
Mark Vernon <vernon@mesatrail.com>
2015-10-26 14:02:24 -0500

    Changing slave_types to a dict for name lookup

--- scale_cluster.bash
+++ scale_cluster.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
-LOCAL_DEPLOY="false"
-PLAN_ONLY="false"
-APPLY_SEPARATE="false"
+LOCAL_DEPLOY=false
+PLAN_ONLY=false
+APPLY_SEPARATE=false
 while [[ $# > 0 ]]
 do
   key="$1"
   case $key in
     --plan_only)
-      PLAN_ONLY="true"
+      PLAN_ONLY=true
     ;;
     --apply_separate)
-      APPLY_SEPARATE="true"
+      APPLY_SEPARATE=true
     ;;
     --local_deploy)
-      LOCAL_DEPLOY="true"
+      LOCAL_DEPLOY=true
     ;;
     *)
       CLUSTER_NAME=$key
@@ -24,9 +24,9 @@ done
 
 source setup_amazon_env.bash $CLUSTER_NAME
 if [ -z "$PLAN_ONLY" ] && [ -z "APPLY_SEPARATE" ] ; then
-    ansible-playbook -v --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pemmanage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY"
+    ansible-playbook --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pemmanage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY"
 else
-    ansible-playbook -v --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pem manage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY" -e "terraform_plan_only=true"
+    ansible-playbook --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pem manage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY" -e "terraform_plan_only=true"
 fi
 
 if [ "$APPLY_SEPARATE" == "true" ] ; then
