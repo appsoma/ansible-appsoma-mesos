1f9f19d92f78788b95fdaba9fde03f0be88155be
Mark Vernon <vernon@mesatrail.com>
2015-10-28 10:18:56 -0500

    Adding extra slave attributes for scaling.

--- scale_cluster.bash
+++ scale_cluster.bash
@@ -1,33 +1,38 @@
 #!/bin/bash
-LOCAL_DEPLOY=false
-PLAN_ONLY=false
+LOCAL_DEPLOY=""
+PLAN_ONLY=""
 APPLY_SEPARATE=false
+ATTRIBUTES=""
 while [[ $# > 0 ]]
 do
   key="$1"
   case $key in
+    --attributes)
+      ATTRIBUTES="-e extra_slave_attributes=$2"
+      shift
+    ;;
     --plan_only)
-      PLAN_ONLY=true
+      PLAN_ONLY="-e terraform_plan_only=true"
     ;;
     --apply_separate)
       APPLY_SEPARATE=true
+      PLAN_ONLY="-e terraform_plan_only=true"
     ;;
     --local_deploy)
-      LOCAL_DEPLOY=true
+      LOCAL_DEPLOY="-e local_slave_deploy=true"
     ;;
     *)
-      CLUSTER_NAME=$key
+      CLUSTER_NAME="$key"
     ;;
   esac
   shift
 done
 
 source setup_amazon_env.bash $CLUSTER_NAME
-if [ -z "$PLAN_ONLY" ] && [ -z "APPLY_SEPARATE" ] ; then
-    ansible-playbook --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pemmanage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY"
-else
-    ansible-playbook --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pem manage_terraform_playbook.yml -e "cluster_name=$CLUSTER_NAME" -e "local_slave_deploy=$LOCAL_DEPLOY" -e "terraform_plan_only=true"
-fi
+
+ansible-playbook --private-key cluster_vars/$CLUSTER_NAME/${CLUSTER_NAME}_key.pem manage_terraform_playbook.yml \
+-e cluster_name=$CLUSTER_NAME $PLAN_ONLY $LOCAL_DEPLOY $ATTRIBUTES
+
 
 if [ "$APPLY_SEPARATE" == "true" ] ; then
   cd terraform/$CLUSTER_NAME
