be00c5a1c48e0fa4c20d2893d35d7e9e4ab3a4e8
Mark Vernon <vernon@mesatrail.com>
2015-11-02 10:42:08 -0600

    Terraform update values.

--- install_ansible.bash
+++ install_ansible.bash
@@ -7,14 +7,14 @@ sudo apt-get install -y ansible unzip
 if [ -z "$1" ]; then
 if [ "$1" != "local" ]; then
 TF_VERSION=0.6.6
-wget https://dl.bintray.com/mitchellh/terraform/terraform_$TF_VERSION_linux_amd64.zip -O terraform_$TF_VERSION.zip
-unzip terraform_$TF_VERSION.zip -d terraform_$TF_VERSION
-sudo cp -P terraform_$TF_VERSION/terraform /usr/local/bin
-sudo cp -P terraform_$TF_VERSION/terraform-provider-aws /usr/local/bin
-sudo cp -P terraform_$TF_VERSION/terraform-provider-google /usr/local/bin
-sudo cp -P terraform_$TF_VERSION/terraform-provider-openstack /usr/local/bin
-sudo cp -P terraform_$TF_VERSION/terraform-provisioner-local-exec /usr/local/bin
-sudo cp -P terraform_$TF_VERSION/terraform-provisioner-file /usr/local/bin
-sudo cp -P terraform_$TF_VERSION/terraform-provisioner-remote-exec /usr/local/bin
+wget https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip -O terraform_${TF_VERSION}.zip
+unzip terraform_${TF_VERSION}.zip -d terraform_${TF_VERSION}
+sudo cp -P terraform_${TF_VERSION}/terraform /usr/local/bin
+sudo cp -P terraform_${TF_VERSION}/terraform-provider-aws /usr/local/bin
+sudo cp -P terraform_${TF_VERSION}/terraform-provider-google /usr/local/bin
+sudo cp -P terraform_${TF_VERSION}/terraform-provider-openstack /usr/local/bin
+sudo cp -P terraform_${TF_VERSION}/terraform-provisioner-local-exec /usr/local/bin
+sudo cp -P terraform_${TF_VERSION}/terraform-provisioner-file /usr/local/bin
+sudo cp -P terraform_${TF_VERSION}/terraform-provisioner-remote-exec /usr/local/bin
 fi
 fi
