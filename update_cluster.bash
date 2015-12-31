d3e1ec97c81a472fa8697f93a00e442e9e14069e
Mark Vernon <vernon@mesatrail.com>
2015-12-31 17:14:13 -0500

    Refactored to separate marathon services from the playbook where marathon is installed.

--- update_cluster.bash
+++ update_cluster.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 source setup_amazon_env.bash $1
 /usr/bin/time -f "wall time to run playbook:%e" \
-ansible-playbook --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py $2 -e "cluster_name=$1"
+ansible-playbook -v --private-key cluster_vars/$1/${1}_key.pem -i inventory/terraform.py $2 -e "cluster_name=$1"
