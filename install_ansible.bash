079ffd6a0d44cb6cda62d1ba62ddb2449705088f
Mark Vernon <vernon@mesatrail.com>
2016-03-16 15:15:48 -0400

    Change installing ansible to use pip

--- install_ansible.bash
+++ install_ansible.bash
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible unzip
+sudo pip install ansible
 
 if [ -z "$1" ]; then
 if [ "$1" != "local" ]; then
