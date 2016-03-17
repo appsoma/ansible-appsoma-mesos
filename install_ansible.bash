151e87ca1029ee602a4798898b50265b19038684
Mark Vernon <vernon@mesatrail.com>
2016-03-17 10:15:04 -0400

    Update to install system ansible (v2.0+)

--- install_ansible.bash
+++ install_ansible.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-sudo pip install ansible
+sudo apt-get install ansible
 
 if [ -z "$1" ]; then
 if [ "$1" != "local" ]; then
-TF_VERSION=0.6.6
+TF_VERSION=0.6.10
 wget https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip -O terraform_${TF_VERSION}.zip
 unzip terraform_${TF_VERSION}.zip -d terraform_${TF_VERSION}
 sudo cp -P terraform_${TF_VERSION}/terraform /usr/local/bin
